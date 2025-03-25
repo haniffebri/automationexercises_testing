f6ead72f324cddc635b60d32bcbce69592e4b7d8
haniffebri <haniffebri31@gmail.com>
2025-03-25 02:07:52 -0400

    improving validation step definition

--- src/test/java/stepdefinitions/RegisterSteps.java
+++ src/test/java/stepdefinitions/RegisterSteps.java
@@ -22,6 +22,8 @@ public class RegisterSteps {
     String invalidEmail;
     String registeredName;
     String unregisteredName;
+
+
     public String generateRandomString(int length) {
         String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"; // Karakter yang digunakan
         StringBuilder randomString = new StringBuilder();
@@ -139,11 +141,6 @@ public class RegisterSteps {
         return email.toString() + "@example.com";
     }
 
-
-    @And("user input unregistered email {string}")
-    public void userInputUnregisteredEmail(String arg0) {
-    }
-
     @And("Verify that ENTER ACCOUNT INFORMATION is visible")
     public void verifyThatENTERACCOUNTINFORMATIONIsVisible() {
     }
@@ -154,17 +151,9 @@ public class RegisterSteps {
         WebDriverWait wait = new WebDriverWait(Utility.getDriver(), Duration.ofSeconds(10));
         WebElement Title = driver.findElement(By.cssSelector("#id_gender1"));
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//b[normalize-space()='Enter Account Information']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#id_gender1")));
         Title.click();
 
-    }
-
-    @And("user input Name {string}")
-    public void userInputName(String arg0) {
-    }
-
-    @And("user input Email {string}")
-    public void userInputEmail(String arg0) {
     }
 
     @And("user input Password")
@@ -322,12 +311,16 @@ public class RegisterSteps {
     }
 
     @And("user click Create Account button")
-    public void userClickCreateAccountButton() {
-
-        WebElement createButton = driver.findElement(By.xpath("//button[normalize-space()='Create Account']"));
-
+    public void userClickCreateAccountButton() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(Utility.getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[normalize-space()='Create Account']")));
+
+        WebElement createButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[normalize-space()='Create Account']")
+        ));
+
+        ((JavascriptExecutor) Utility.getDriver()).executeScript("arguments[0].scrollIntoView(true);", createButton);
+        Thread.sleep(500);
+
         createButton.click();
 
     }
@@ -386,6 +379,7 @@ public class RegisterSteps {
     @Then("Verify that Email Address already exist! is visible")
     public void verifyThatEmailAddressAlreadyExistIsVisible() throws InterruptedException {
         try {
+            Thread.sleep(2000);
             WebElement errorMessage = driver.findElement(By.xpath("//p[normalize-space()='Email Address already exist!']"));
 
             String actualText = errorMessage.getText().trim();
@@ -397,22 +391,6 @@ public class RegisterSteps {
             System.out.println("Error message not found!");
         }
         Utility.quitDriver();
-    }
-
-    @And("Click on Signup/Login button")
-    public void clickOnSignupLoginButton() {
-    }
-
-    @When("Fill name")
-    public void fillName() {
-    }
-
-    @And("Fill email registered")
-    public void fillEmailRegistered() {
-    }
-
-    @And("Click Signup button")
-    public void clickSignupButton() {
     }
 
     @And("Fill email with invalid format \\(missing @example.com) {string}")
@@ -566,6 +544,165 @@ public class RegisterSteps {
 
         System.out.println("Validation Message : " + validationMessage);
         Utility.quitDriver();
+
+    }
+
+    @And("user input unregistered email {string}")
+    public void userInputUnregisteredEmail(String Email) throws InterruptedException {
+
+        WebElement fillEmail = driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
+        fillEmail.sendKeys(Email);
+        Thread.sleep(3000);
+
+    }
+
+    @When("user input unregistered name {string}")
+    public void userInputUnregisteredName(String Name) {
+
+        WebElement fillName = driver.findElement(By.xpath("//input[@placeholder='Name']"));
+        fillName.sendKeys(Name);
+
+    }
+
+    @When("user input long character unregistered name")
+    public void userInputLongCharacterUnregisteredName() {
+        String Name = generateRandomString(100);
+        WebElement unregisNameField = driver.findElement(By.xpath("//input[@placeholder='Name']"));
+        unregisNameField.sendKeys(Name);
+
+        unregisteredName = Name;
+        System.out.println("Unregistered Name: " + unregisteredName);
+    }
+
+    @And("user input long character unregistered email")
+    public void userInputLongCharacterUnregisteredEmail() {
+
+        String longRandomEmail = generateLongRandomEmail();
+
+        WebElement fillEmail = driver.findElement(By.xpath("//input[@data-qa='signup-email']"));
+        fillEmail.sendKeys(longRandomEmail);
+
+        System.out.println("Generated Email : " + longRandomEmail);
+    }
+
+    public String generateLongRandomEmail() {
+        String chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+        StringBuilder localPart = new StringBuilder();
+        StringBuilder domain = new StringBuilder();
+        StringBuilder extension = new StringBuilder();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 33; i++) {
+            localPart.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+
+        for (int i = 0; i < 33; i++) {
+            domain.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+
+        for (int i = 0; i < 33; i++) {
+            extension.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+
+        return localPart + "@" + domain + "." + extension;
+    }
+
+    @And("user input long character Password")
+    public void userInputLongCharacterPassword() {
+
+        String password = generateRandomString(100); // Generate password 10 karakter
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='password']"));
+        passwordField.sendKeys(password);
+        System.out.println("Generated Password: " + password);
+
+    }
+
+    @And("user input long character First name")
+    public void userInputLongCharacterFirstName() {
+
+        String firstName = generateRandomString(100);
+        WebElement firstNameField = driver.findElement(By.xpath("//input[@id='first_name']"));
+        firstNameField.sendKeys(firstName);
+        System.out.println("Generated First Name: " + firstName);
+
+    }
+
+    @And("user input long character Last name")
+    public void userInputLongCharacterLastName() {
+
+        String lastName = generateRandomString(100);
+        WebElement lastNameField = driver.findElement(By.xpath("//input[@id='last_name']"));
+        lastNameField.sendKeys(lastName);
+        System.out.println("Generated Last Name: " + lastName);
+
+    }
+
+    @And("user input long character Company")
+    public void userInputLongCharacterCompany() {
+
+        String company = "Company-" + generateRandomString(100);
+        WebElement companyField = driver.findElement(By.xpath("//input[@id='company']"));
+        companyField.sendKeys(company);
+        System.out.println("Generated Company Name: " + company);
+
+    }
+
+    @And("user input long character Address")
+    public void userInputLongCharacterAddress() {
+
+        String address = generateRandomString(12) + " Street";
+        WebElement addressField = driver.findElement(By.cssSelector("#address1"));
+        addressField.sendKeys(address);
+        System.out.println("Generated Address: " + address);
+
+    }
+
+    @And("user input long character Address2")
+    public void userInputLongCharacterAddress2() {
+
+        String address2 = generateRandomString(100) + " Street";
+        WebElement addressField = driver.findElement(By.cssSelector("#address2"));
+        addressField.sendKeys(address2);
+        System.out.println("Generated Address 2: " + address2);
+    }
+
+    @And("user input long character State")
+    public void userInputLongCharacterState() {
+
+        String state = generateRandomString(100);
+        WebElement stateField = driver.findElement(By.xpath("//input[@id='state']"));
+        stateField.sendKeys(state);
+        System.out.println("Generated State: " + state);
+
+    }
+
+    @And("user input long character City")
+    public void userInputLongCharacterCity() {
+
+        String city = generateRandomString(100);
+        WebElement cityField = driver.findElement(By.xpath("//input[@id='city']"));
+        cityField.sendKeys(city);
+        System.out.println("Generated City: " + city);
+
+    }
+
+    @And("user input long character Zipcode")
+    public void userInputLongCharacterZipcode() {
+
+        String zipcode = generateRandomString(100);
+        WebElement zipcodeField = driver.findElement(By.xpath("//input[@id='zipcode']"));
+        zipcodeField.sendKeys(zipcode);
+        System.out.println("Generated Zipcode: " + zipcode);
+
+    }
+
+    @And("user input long character Mobile Number")
+    public void userInputLongCharacterMobileNumber() {
+
+        String mobileNumber = "+62" + generateRandomString(100);
+        WebElement mobileField = driver.findElement(By.xpath("//input[@id='mobile_number']"));
+        mobileField.sendKeys(mobileNumber);
+        System.out.println("Generated Mobile Number: " + mobileNumber);
 
     }
 }
--- src/test/resources/features/register/Register.feature
+++ src/test/resources/features/register/Register.feature
@@ -1,5 +1,14 @@
 Feature: Register
 
+  Scenario: Register without filling email and name
+    Given launched browser
+    And navigate to homepage
+    And verify that home page is visible successfully
+    And user click on Signup button
+    And Verify New User Signup! is visible
+    And user click Signup button
+    Then Verify Please fill out this field. is visible on the name field
+
   Scenario: Register without filling email
     Given launched browser
     And navigate to homepage
@@ -20,6 +29,17 @@ Feature: Register
     And user click Signup button
     Then Verify Please fill out this field. is visible on the name field
 
+  Scenario: Register using non-English characters
+    Given launched browser
+    And navigate to homepage
+    And verify that home page is visible successfully
+    And user click on Signup button
+    And Verify New User Signup! is visible
+    When user input unregistered name "张伟张伟张伟张伟张伟张伟张伟张伟张伟"
+    And user input unregistered email "张伟张伟张伟张伟张伟@أحمدأحمد.أحمدأحمد"
+    And user click Signup button
+    Then Verify Please fill out this field. is visible on the name field
+
   Scenario: Register user with unregistered email
     Given launched browser
     And navigate to homepage
@@ -28,6 +48,36 @@ Feature: Register
     And Verify New User Signup! is visible
     When user input registered name "yourName"
     And user input unregistered email
+    And user click Signup button
+    And Verify that ENTER ACCOUNT INFORMATION is visible
+    And user select Title
+    And user input Password
+    And user select Date of birth
+    And user select checkbox Sign up for our newsletter!
+    And user select checkbox Receive special offers from our partners!
+    And user input First name
+    And user input Last name
+    And user input Company
+    And user input Address
+    And user input Address2
+    And user input Country
+    And user input State
+    And user input City
+    And user input Zipcode
+    And user input Mobile Number
+    And user click Create Account button
+    Then Verify that ACCOUNT CREATED! is visible
+    And user click Continue button
+    And Verify that Logged in as registered username is visible
+
+  Scenario: Register using same email but different casing
+    Given launched browser
+    And navigate to homepage
+    And verify that home page is visible successfully
+    And user click on Signup button
+    And Verify New User Signup! is visible
+    When user input registered name "yourName"
+    And user input unregistered email "dsfgfbrwbwds@rebebfdbbdbrebebe.com"
     And user click Signup button
     And Verify that ENTER ACCOUNT INFORMATION is visible
     And user select Title
@@ -91,15 +141,45 @@ Feature: Register
     And user click Continue button
     And Verify that Logged in as unregistered username is visible
 
+  Scenario: Register with whitespace around name or email
+    Given launched browser
+    And navigate to homepage
+    And verify that home page is visible successfully
+    And user click on Signup button
+    And Verify New User Signup! is visible
+    When user input unregistered name " qqq "
+    And user input unregistered email " vdvdfv@dvdfvdfr.reggbdfbdfg "
+    And user click Signup button
+    And Verify that ENTER ACCOUNT INFORMATION is visible
+    And user select Title
+    And user input Password
+    And user select Date of birth
+    And user select checkbox Sign up for our newsletter!
+    And user select checkbox Receive special offers from our partners!
+    And user input First name
+    And user input Last name
+    And user input Company
+    And user input Address
+    And user input Address2
+    And user input Country
+    And user input State
+    And user input City
+    And user input Zipcode
+    And user input Mobile Number
+    And user click Create Account button
+    Then Verify that ACCOUNT CREATED! is visible
+    And user click Continue button
+    And Verify that Logged in as unregistered username is visible
+
   Scenario: Register user with registered email
     Given launched browser
     And navigate to homepage
     And verify that home page is visible successfully
     And user click on Signup button
     And Verify New User Signup! is visible
-    When Fill name
-    And Fill email registered
-    And Click Signup button
+    When user input unregistered name
+    And user input registered email "ex4arfxc@example.com"
+    And user click Signup button
     Then Verify that Email Address already exist! is visible
 
   Scenario: Register user with registered name
@@ -109,7 +189,7 @@ Feature: Register
     And user click on Signup button
     And Verify New User Signup! is visible
     When user input registered name "yourName"
-    And user input unregistered email "yourEmail"
+    And user input unregistered email
     And user click Signup button
     And Verify that ENTER ACCOUNT INFORMATION is visible
     And user select Title
@@ -140,7 +220,7 @@ Feature: Register
     And Verify New User Signup! is visible
     When user input registered name "yourName"
     And Fill email with invalid format (missing @example.com) "example"
-    And Click Signup button
+    And user click Signup button
     Then Verify that Please include an @ in the email address. email is missing an @. is visible
 
 
@@ -560,6 +640,36 @@ Feature: Register
     And user input Zipcode
     And user click Create Account button
     Then Verify that Please fill out this field is visible on the Mobile Number field
+
+  Scenario: Register with long input values
+    Given launched browser
+    And navigate to homepage
+    And verify that home page is visible successfully
+    And user click on Signup button
+    And Verify New User Signup! is visible
+    When user input long character unregistered name
+    And user input long character unregistered email
+    And user click Signup button
+    And Verify that ENTER ACCOUNT INFORMATION is visible
+    And user select Title
+    And user input long character Password
+    And user select Date of birth
+    And user select checkbox Sign up for our newsletter!
+    And user select checkbox Receive special offers from our partners!
+    And user input long character First name
+    And user input long character Last name
+    And user input long character Company
+    And user input long character Address
+    And user input long character Address2
+    And user input Country
+    And user input long character State
+    And user input long character City
+    And user input long character Zipcode
+    And user input long character Mobile Number
+    And user click Create Account button
+    Then Verify that ACCOUNT CREATED! is visible
+    And user click Continue button
+    And Verify that Logged in as unregistered username is visible
 
   Scenario: Register with special characters only in name field
     Given launched browser
